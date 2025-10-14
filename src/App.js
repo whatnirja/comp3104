791c2a32a507e36c28f35c93fcfaf6dbb151be90
nirja <nirjadabhi34@gmail.com>
2025-10-13 23:52:22 -0400

    name changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101509539 || Nirja || DevOps</h5>
+      <h5>Developed By: 101509539 || Nirja Dabhigit  || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
